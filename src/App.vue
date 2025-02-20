9a73f30e2ee76623144c05072df4f68030e2e603
Ase444ka <istomina.asia@yandex.ru>
2025-02-20 20:59:34 +0300

    refactor: add cover and header components

--- src/App.vue
+++ src/App.vue
@@ -1,135 +1,20 @@
 <script setup>
+import MyCover from '@/components/MyCover.vue'
+import MyHeader from '@/components/MyHeader.vue'
 import {RouterLink, RouterView} from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <div class="container">
-    <header>
-      <svg>
-        <use href="@/assets/sprites.svg#logo"></use>
-      </svg>
-      <nav>
-        <ul>
-          <li><RouterLink>Home</RouterLink></li>
-          <li><RouterLink>Blog</RouterLink></li>
-          <li><RouterLink>About Us</RouterLink></li>
-          <li><RouterLink>Contact us</RouterLink></li>
-        </ul>
-        <button class="button_alternate">Subscribe</button>
-      </nav>
-    </header>
-    <main>
-      <div class="cover">
-        <div class="container">
-          <figure class="cover__img">
-            <img src="@/assets/images/man-in-black.png" />
-          </figure>
-          <div class="cover__subtitle">Posted on <a href="#">startup</a></div>
-          <h2 class="cover__title display">
-            Step-by-step guide to choosing great font pairs
-          </h2>
-          <div class="cover__info">
-            By <a href="#">James West</a
-            ><time datetime="2022-05-23">May 23, 2022</time>
-          </div>
-          <p class="cover__text">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident.
-          </p>
-          <button class="cover__button">Read More ></button>
-        </div>
-      </div>
-    </main>
-  </div>
+  <MyHeader />
+  <main>
+    <MyCover />
+  </main>
 
   <RouterView />
 </template>
 
 <style scoped lang="scss">
-header {
-  padding: 13px 80px 12px;
-  color: var(--white);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  svg {
-    width: 150px;
-    height: 30px;
-  }
-}
-nav {
-  display: flex;
-  gap: 31px;
-}
-ul {
-  display: flex;
-  gap: 27px;
-  padding-top: 14px;
-}
-.cover {
-  position: relative;
-  overflow: hidden;
-  height: 720px;
-  padding: 123px 80px;
-  color: var(--white);
-  &__img {
-    position: absolute;
-    display: block;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: -10;
-    &::before {
-      content: '';
-      z-index: -1;
-      width: 100%;
-      height: 100%;
-      display: block;
-      position: absolute;
-      z-index: 10;
-      background: radial-gradient(
-        30.56% 76.04% at 74.58% 0%,
-        rgba(0, 0, 0, 0) 0%,
-        rgba(0, 0, 0, 0.6) 100%
-      );
-    }
-  }
-  &__subtitle {
-    text-transform: uppercase;
-    letter-spacing: 2.9px;
-    a {
-      font-weight: 600;
-      font-size: 0.95em;
-      letter-spacing: 4.4px;
-    }
-  }
-  &__title {
-    padding-top: 21px;
-    width: 70%;
-  }
-  &__info {
-    padding-top: 24px;
-    time {
-      border-left: 1px solid var(--white);
-      margin-left: 14px;
-      padding-left: 10px;
-      letter-spacing: 0.5px;
-    }
-  }
-  &__title {
-    padding-top: 19px;
-  }
-  &__text {
-    padding-top: 16px;
-    width: 45%;
-    letter-spacing: 0.33px;
-  }
-  &__button {
-    margin-top: 48px;
-    margin-left: -2px;
-  }
-}
+
+
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="header__inner">
+        <svg>
+          <use href="@/assets/sprites.svg#logo"></use>
+        </svg>
+        <nav>
+          <ul>
+            <li><RouterLink>Home</RouterLink></li>
+            <li><RouterLink>Blog</RouterLink></li>
+            <li><RouterLink>About Us</RouterLink></li>
+            <li><RouterLink>Contact us</RouterLink></li>
+          </ul>
+          <button class="button_alternate">Subscribe</button>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  color: var(--white);
+  .header__inner {
+    padding: 13px 80px 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  svg {
+    width: 150px;
+    height: 30px;
+  }
+}
+nav {
+  display: flex;
+  gap: 31px;
+}
+ul {
+  display: flex;
+  gap: 27px;
+  padding-top: 14px;
+}
+</style>
